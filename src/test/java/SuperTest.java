e8808cbb27b12cc92d5c755052e11e3a40b74e36
Artem <medvynskyi.artem@gmail.com>
2019-07-18 18:02:02 +0300

    v2 Tests of URL and Title

--- src/test/java/SuperTest.java
+++ src/test/java/SuperTest.java
@@ -19,7 +19,7 @@ public class SuperTest {
     private String url2 = "http://nnmclub.to/";
     private String url3 = "http://nnmclub.me/";
     private String url4 = "https://n.iss.one/";
-    private String[] urls = {"https://n.iss.one/","http://nnmclub.me/","http://nnmclub.to/"};
+
     private String titleSite = "Торрент-трекер :: NNM-Club";
 
     // 1. открываем google.ru
@@ -33,41 +33,38 @@ public class SuperTest {
     public void PreCondition(){
        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
        driver = new ChromeDriver();
-       driver.get(url);
     }
 
     @Test (priority = 1)
     public void myTestSearch() {
-
+        driver.get(url);
         driver.findElement(By.cssSelector("#tsf [type=\"text\"]")).sendKeys(k);
         driver.findElement(By.cssSelector("div.FPdoLc.VlcLAe input[type=\"submit\"]:nth-child(1)")).click();
         WebElement a = driver.findElement(By.xpath("//*[text()='NNM-Club: Торрент-трекер']"));
+
         a.click();
         for (String tab : driver.getWindowHandles()) {
             driver.switchTo().window(tab);
         }
     }
 
-    @Test
+    @Test (priority = 2)
     public void myTestGetUrl(){
-       /* for (String u : urls) {
-            WebDriverWait wait = (new WebDriverWait(driver, 5));
-            wait.until(ExpectedConditions.urlToBe(u));
-            System.out.println(u);
-            System.out.println(driver.getCurrentUrl());
-        } */
-        WebDriverWait wait = (new WebDriverWait(driver, 5));
+
+        WebDriverWait wait = (new WebDriverWait(driver, 10));
         wait.until(ExpectedConditions.urlToBe(url2));
-        //System.out.println(u);
+
         System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getCurrentUrl(), url2);
     }
 
-    @Test
+    @Test (priority = 3)
     public void myTestGetTitle(){
+        WebDriverWait wait = (new WebDriverWait(driver, 2));
+        wait.until(ExpectedConditions.urlToBe(url2));
 
         System.out.println(driver.getTitle());
-        Assert.assertEquals(titleSite, driver.getTitle());
-
+        Assert.assertEquals(driver.getTitle(), titleSite);
     }
 
     @AfterTest
